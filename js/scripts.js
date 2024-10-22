c89b58a98a53f3968d2b9b43e0f659be65297ff4
Ayu Branding <ayubranding@gmail.com>
2024-10-22 14:25:07 +0200

    adding responsive to navbar

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,10 @@
 /*This is the file with the code and programming part of the website: like the movements*/
 
+document.addEventListener("DOMContentLoaded", function() {
+    const navbar = document.querySelector("[data-navbar]");
+    const hamburger = document.querySelector(".hamburger");
+
+    hamburger.addEventListener("click", function() {
+        navbar.classList.toggle("active"); // Alterna la clase 'active' para mostrar u ocultar el menú
+    });
+});
