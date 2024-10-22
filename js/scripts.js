ac12b1e5afb0d3d9d4d8fddab15542a2a6f6d732
Ayu Branding <ayubranding@gmail.com>
2024-10-22 15:34:53 +0200

    buttom

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,8 @@
-/*This is the file with the code and programming part of the website: like the movements*/
-
 document.addEventListener("DOMContentLoaded", function() {
-    const navbar = document.querySelector("[data-navbar]");
+    const navbar = document.querySelector("[data-navbar] .navbar-list");
     const hamburger = document.querySelector(".hamburger");
 
     hamburger.addEventListener("click", function() {
-        navbar.classList.toggle("active"); // Alterna la clase 'active' para mostrar u ocultar el menú
+        navbar.classList.toggle("active"); // Alterna la clase 'active'
     });
 });
